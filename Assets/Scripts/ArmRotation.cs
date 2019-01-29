5c84a9ab4eb3ae13fea29527756e8cceeb6bc58d
emr <renquin@awtce.be>
2019-01-29 07:55:39 +0100

    Episode 5: new character (Astronaut) + arm rotation.

---
+++ Assets/Scripts/ArmRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmRotation : MonoBehaviour
+{
+
+    public int rotationOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg; // find the angle in degrees
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset);
+    }
+}
