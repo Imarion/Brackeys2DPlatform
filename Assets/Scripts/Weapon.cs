cdfa98ff0248fdddd06c72af7f5bb4943b522241
emr <renquin@awtce.be>
2019-01-30 12:46:10 +0100

    Episode 9 : bullet effect trail

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,9 +6,13 @@ public class Weapon : MonoBehaviour
 {
 
     public float fireRate = 0f; // 0 = single fire
+    public float effectSpawnRate = 10f;
     public float damage = 10;
     public LayerMask whatToHit;
 
+    public Transform BulletTrailPrefab;
+
+    float TimeToSpawnEffect = 0f;
     float timeToFire = 0f;
     Transform firepoint;
 
@@ -49,11 +53,21 @@ public class Weapon : MonoBehaviour
         Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
         Vector2 firePointPosition = new Vector2(firepoint.position.x, firepoint.position.y);
         RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
+
+        if (Time.time >= TimeToSpawnEffect) {
+            Effect();
+            TimeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+        }
+
         Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.cyan);
 
         if (hit.collider != null) {
             Debug.DrawLine(firePointPosition, hit.point, Color.red);
             Debug.Log("We hit " + hit.collider.name + " and did " + damage + " damage." );
         }
+    }
+
+    void Effect() {
+        Instantiate(BulletTrailPrefab, firepoint.position, firepoint.rotation);
     }
 }
