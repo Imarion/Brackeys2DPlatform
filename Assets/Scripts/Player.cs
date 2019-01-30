559fd98edc848ac5edddaa39d19c8ba91e64605c
emr <renquin@awtce.be>
2019-01-30 17:37:07 +0100

    Episode 11: player health + clamp camera + destroy player when he falls.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [System.Serializable]
+    public class PlayerStats {
+        public int Health = 100;
+    }
+
+    public int fallBoundary = -20;
+
+    public PlayerStats playerStats = new PlayerStats();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= fallBoundary) {
+            DamagePlayer(9999999);
+        }
+    }
+
+    public void DamagePlayer(int damage) {
+        playerStats.Health -= damage;
+
+        if (playerStats.Health <= 0) {
+            GameMaster.KillPlayer(this);
+        }
+    }
+}
