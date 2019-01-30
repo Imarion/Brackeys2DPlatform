3e7fd1324864e64a3b35565b8a5dfee3041f36a2
emr <renquin@awtce.be>
2019-01-30 08:28:33 +0100

    Episodes 8A and 8B: weapon fire.

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public float fireRate = 0f; // 0 = single fire
+    public float damage = 10;
+    public LayerMask whatToHit;
+
+    float timeToFire = 0f;
+    Transform firepoint;
+
+    void Awake()
+    {
+        firepoint = transform.Find("FirePoint");
+        if (firepoint == null) {
+            Debug.LogError("No firepoint attached to the weapon !");
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fireRate == 0f)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else {
+            if (Input.GetButton("Fire1") && Time.time > timeToFire) {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot() {
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firepoint.position.x, firepoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
+        Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.cyan);
+
+        if (hit.collider != null) {
+            Debug.DrawLine(firePointPosition, hit.point, Color.red);
+            Debug.Log("We hit " + hit.collider.name + " and did " + damage + " damage." );
+        }
+    }
+}
